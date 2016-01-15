8d9c746ce810db79a3149191ee9875092c34a9ad
pfory <pfory@seznam.cz>
2016-01-15 14:42:09 +0100

    +nacitani teploty pres I2C

--- seg7/seg7.ino
+++ seg7/seg7.ino
@@ -1,8 +1,11 @@
 #include <SevSeg.h>
+#include <wire.h>
 
 SevSeg sevseg; //Instantiate a seven segment controller object
-byte counter=0;
+/*byte counter=0;
 long lastMillis=0;
+*/
+byte temperature;
 
 void setup()
 {
@@ -16,16 +19,25 @@ void setup()
                             //Therefore, for a 4-digit 7-segment + pd, COMMON_ANODE display, the max update rate for a "brightness" of 100 is 1/(2000us*8) = 62.5Hz.
                             //I am choosing a "brightness" of 10 because it increases the max update rate to approx. 1/(200us*8) = 625Hz.
                             //This is preferable, as it decreases aliasing when recording the display with a video camera....I think.
+  
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
 }
 
 void loop()
 {
-  if (millis()-lastMillis>=1000) {
+/*  if (millis()-lastMillis>=1000) {
     lastMillis=millis();
     sevseg.setNumber(counter++, 0);
     if (counter==100)
       counter=0;
   }
-
+*/
+  sevseg.setNumber(temperature, 0);
   sevseg.refreshDisplay(); // Must run repeatedly; don't use blocking code (ex: delay()) in the loop() function or this won't work right
+}
+
+
+void receiveEvent(int howMany) {
+  temperature = Wire.read();    // receive byte as an integer
 }
